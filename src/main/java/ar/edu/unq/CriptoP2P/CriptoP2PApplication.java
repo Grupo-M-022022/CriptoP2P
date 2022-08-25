37ce7d46b3aa32881ecabe634a063e85b2837f8d
Pablo T <trochepablodaniel@gmail.com>
2022-08-24 21:19:05 -0300

    init

---
+++ src/main/java/ar/edu/unq/CriptoP2P/CriptoP2PApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.CriptoP2P;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptoP2PApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriptoP2PApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unq/CriptoP2P/CriptoP2PApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.CriptoP2P;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptoP2PApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
