29ce9517ad5cafc7d0b8a8f078139353ed9857dc
Fernando <77fernandoroldan77@gmail.com>
2022-09-01 17:06:19 -0300

    Agregados del modelo Usuario y Criptomonedas

--- src/main/java/ar/edu/unq/CriptoP2P/model/Usuario.java
+++ src/main/java/ar/edu/unq/CriptoP2P/model/Usuario.java
@@ -1,11 +1,18 @@
 package ar.edu.unq.CriptoP2P.model;
 
 public class Usuario {
-	public Usuario(String email, String password){
+	public Usuario(String email, String password, String direccion, Integer CVUMercadoPago,
+				   String direccionBilleteraDeCriptoActivos) {
+		
 		setEmail(email);
 		setPassword(password);
+		setDireccion(direccion);
+		setCVUMercadoPago(CVUMercadoPago); 										  //22 digitos
+		setDireccionBilleteraDeCriptoActivos(direccionBilleteraDeCriptoActivos);  //8 digitos
+		
+		
 	}
-	
+
 	public String getEmail() {
 		return email;
 	}
@@ -21,9 +28,34 @@ public class Usuario {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	public String getDireccion() {
+		return direccion;
+	}
+	
+	public void setDireccion(String dir) {
+		this.direccion = dir;
+	}
+	public Integer getCVUMercadoPago() {
+		return CVUMercadoPago;
+	}
+	
+	public void setCVUMercadoPago(Integer cvu) {
+		this.CVUMercadoPago = cvu ;
+	}
+	
+	public String getDireccionBilleteraDeCriptoActivos() { //8 digitos
+		return direccionBilleteraDeCriptoActivos;
+	}
+	public void setDireccionBilleteraDeCriptoActivos(String dbc) {
+		this.direccionBilleteraDeCriptoActivos = dbc;
+	}
+	
 	private String email;
 	private String password;
-	
-	
+	private String direccion;
+	private Integer CVUMercadoPago;
+	private String direccionBilleteraDeCriptoActivos;
+
 }
+
+
