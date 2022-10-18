0dbf90d4b5add2cb00ee46e511e42b70fe318d8c
Pablo T <trochepablodaniel@gmail.com>
2022-10-18 12:27:36 -0300

    get criptoActivos de base local

--- src/main/java/ar/edu/unq/criptop2p/persistence/Imp/CotizacionBinanceBinanceRepository.java
+++ src/main/java/ar/edu/unq/criptop2p/persistence/Imp/CotizacionBinanceBinanceRepository.java
@@ -1,7 +1,11 @@
 package ar.edu.unq.criptop2p.persistence.Imp;
 
+import ar.edu.unq.criptop2p.model.entity.Cotizacion;
 import ar.edu.unq.criptop2p.model.entity.CotizacionBinance;
+import ar.edu.unq.criptop2p.model.entity.CriptoMoneda;
 import ar.edu.unq.criptop2p.persistence.interfaces.ICotizacionBinanceRepository;
+import ar.edu.unq.criptop2p.persistence.interfaces.ICriptoModedaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
@@ -10,28 +14,16 @@ import java.util.List;
 
 @Repository
 public class CotizacionBinanceBinanceRepository implements ICotizacionBinanceRepository {
+    @Autowired
+    private ICriptoModedaRepository criptoModedaRepository;
     private static final String uri = "https://api1.binance.com/api/v3/ticker/price?symbol=";
-    private final String[] criptos = new String[] {   "ALICEUSDT",
-                                        "MATICUSDT",
-                                        "AXSUSDT",
-                                        "AAVEUSDT",
-                                        "ATOMUSDT",
-                                        "NEOUSDT",
-                                        "DOTUSDT",
-                                        "ETHUSDT",
-                                        "CAKEUSDT",
-                                        "BTCUSDT",
-                                        "BNBUSDT",
-                                        "ADAUSDT",
-                                        "TRXUSDT",
-                                        "AUDIOUSDT"};
-
 
     public List<CotizacionBinance> getCotizaciones() {
         RestTemplate restTemplate = new RestTemplate();
         List<CotizacionBinance> cotizaciones = new ArrayList<>();
-        for (String cripto : criptos) {
-            ResponseEntity<CotizacionBinance> response = restTemplate.getForEntity(uri + cripto, CotizacionBinance.class);
+        List<CriptoMoneda> criptoActivos = criptoModedaRepository.findAll();
+        for (CriptoMoneda cripto : criptoActivos) {
+            ResponseEntity<CotizacionBinance> response = restTemplate.getForEntity(uri + cripto.getNombre(), CotizacionBinance.class);
             cotizaciones.add(response.getBody());
         }
         return cotizaciones;
