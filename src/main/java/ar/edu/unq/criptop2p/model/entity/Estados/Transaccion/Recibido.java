92812b0ffe7d79178c7efdf8f8208631db33c9d5
Pablo T <trochepablodaniel@gmail.com>
2022-10-23 11:10:50 -0300

    remove estado no utilizado

--- src/main/java/ar/edu/unq/criptop2p/model/entity/Estados/Transaccion/Recibido.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unq.criptop2p.model.entity.Estados.Transaccion;
-
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-
-@Entity
-@DiscriminatorValue("2")
-public class Recibido extends EstadoTransaccion {
-}
