7fb27290c681f568fd6cfdda3d6e892cbe2e4cad
Pablo T <trochepablodaniel@gmail.com>
2022-10-25 22:38:04 -0300

    fix test controller end-to-end usuario

--- src/test/java/ar/edu/unq/criptop2p/UsuarioControllerTest.java
+++ src/test/java/ar/edu/unq/criptop2p/UsuarioControllerTest.java
@@ -10,13 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
+import java.util.Objects;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class UsuarioControllerTest extends CriptoP2PApplicationTests {
-    private static final String HTTP_LOCALHOST = "http://localhost:8080/usuario/usuarios";
-    //agregado el request del swagger
-    private static final String HTTP_LOCALHOST_USUARIOID        = "http://localhost:8080/usuario/15" ;
-    private static final String HTTP_LOCALHOST_USUARIOREGISTRAR =   "http://localhost:8080/usuario/registrar";
+    private static final String HTTP_LOCALHOST = "http://localhost:";
 
     @Autowired
     private IUsuarioService usuarioService;
@@ -32,43 +31,31 @@ public class UsuarioControllerTest extends CriptoP2PApplicationTests {
 
     @Test
     public void verUsuariosTest() throws Exception {
-        assertThat(this.restTemplate.getForObject(HTTP_LOCALHOST + port + "/usuario/usuarios",
-                String.class)).contains("/usuarios");
+        var listaUsuarios = Objects.requireNonNull(this.restTemplate.getForEntity(HTTP_LOCALHOST + port + "/usuario/usuarios", UsuarioDTO[].class)).getBody();
+        assert listaUsuarios != null;
+        assertThat(listaUsuarios.length).isGreaterThan(0);
     }
 
     @Test
     public void verUsuarioRegistradoTest() throws Exception {
-
-
         UsuarioDTO usuarioRegistrado = new UsuarioDTO("userRegistrado@gmail.com", "Tucuman 1000", "1234",
                 "1234567891234567891234", "12345678");
-        usuarioRegistrado.setId(10);
-        usuarioService.save(usuarioRegistrado);
-        usuarioController.registrar(usuarioRegistrado);
-
-        assertThat(this.restTemplate.getForObject(HTTP_LOCALHOST_USUARIOREGISTRAR + port + "/usuario/registrar",
-                   UsuarioDTO.class).getId()).isEqualTo(10L);
-        //Todo: Puse usuarioDTO porque es lo que pide el assertThat, intente con la variable usuarioControler pero no se puede no se porque
-        //Todo: (pide .class) obviamente no funciona porque el usuarioDTO tiene 0 de ID
-
+        var usuarioDTO = usuarioService.save(usuarioRegistrado);
+        var usuarioEndToEnd = this.restTemplate.getForEntity(HTTP_LOCALHOST + port + "/usuario/" + usuarioDTO.getId(), UsuarioDTO.class);
+        var idUsuarioCreado = Objects.requireNonNull(usuarioEndToEnd.getBody()).getId();
+        assertThat(idUsuarioCreado).isNotZero();
     }
+
     @Test
     public void verSiExisteElUsuarioConID15() throws Exception {
-
-        //Ya estan cargados mediante datos de prueba algunos usuarios, uno es con Id 15
-
-        assertThat(this.restTemplate.getForObject(HTTP_LOCALHOST_USUARIOID + port + "/usuario/id",
-                String.class)).contains("15");
-
+        var idUsuario = this.restTemplate.getForObject(HTTP_LOCALHOST + port + "/usuario/" + "15", UsuarioDTO.class).getId();
+        assertThat(idUsuario).isEqualTo(15);
     }
+
     @Test
     public void verQueNoExisteUnUsuarioConID14() throws Exception {
-
-        //Ya estan cargados mediante datos de prueba algunos usuarios ninguno con ID14
-
-        assertThat(this.restTemplate.getForObject(HTTP_LOCALHOST_USUARIOID + port + "/usuario/id",
+        assertThat(this.restTemplate.getForObject(HTTP_LOCALHOST + port + "/usuario/id",
                 String.class)).doesNotContain("14");
-
     }
 
 }
